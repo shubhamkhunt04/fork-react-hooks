e7aaea11b4dcb2b805cef7b6e5d3e94232ad7b95
shubhamkhunt04 <shubhamkhunt08@gmail.com>
2021-04-18 11:57:22 +0530

    useDarkMode hook added

---
+++ hooks/useDarkMode.js
@@ -0,0 +1,25 @@
+import { useEffect } from 'react';
+import { useLocalStorage } from '.';
+
+const useDarkMode = (customStyle) => {
+  const [mode, setMode] = useLocalStorage('mode', 'light');
+  useEffect(() => {
+    if (mode === 'dark') {
+      document.body.style.backgroundColor = '#0D1117';
+      document.body.style.color = 'white';
+    } else {
+      document.body.style.backgroundColor = 'white';
+      document.body.style.color = 'black';
+    }
+
+    if (customStyle && mode === 'dark') {
+      Object.assign(document.body.style, customStyle);
+    }
+
+    // Only call again if customStyle object and mode reference changes
+  }, [customStyle, mode]);
+
+  return [mode, setMode];
+};
+
+export default useDarkMode;
--- index.js
+++ index.js
@@ -3,5 +3,13 @@ import useFetch from './hooks/useFetch';
 import useForm from './hooks/useForm';
 import useOnlineStatus from './hooks/useOnlineStatus';
 import useLocalStorage from './hooks/useLocalStorage';
+import useDarkMode from './hooks/useDarkMode';
 
-export { useToggle, useFetch, useForm, useOnlineStatus, useLocalStorage };
+export {
+  useToggle,
+  useFetch,
+  useForm,
+  useOnlineStatus,
+  useLocalStorage,
+  useDarkMode,
+};
